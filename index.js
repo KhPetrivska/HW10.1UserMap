8dc3fa455296785a0500c8666bc7e1a2c33213be
Kristina <kristin.petrivska@gmail.com>
2024-08-05 23:20:58 -0600

    update message

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const users = {
   nationality: "indian",
   returnData: function () {
     console.log(
-      `This is ${this.name} ${this.secondName}. Age is: ${this.age}. Citizenship: ${this.citizenship}. Nationality: ${this.nationality}.`
+      `This is ${this.name} ${this.secondName} from ${this.city}. Age is: ${this.age}. Citizenship: ${this.citizenship}. Nationality: ${this.nationality}.`
     );
   },
 };
